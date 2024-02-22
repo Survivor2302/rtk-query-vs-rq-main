04dc88cc0c6a0ccb12bf1eea837612469c3241ef
Léo Bournizien <73833842+Survivor2302@users.noreply.github.com>
2024-02-22 09:29:04 +0100

    Update supabase.ts

--- rtk-todos/src/lib/supabase.ts
+++ rtk-todos/src/lib/supabase.ts
@@ -7,6 +7,6 @@
 // Compare this snippet from rtk-todos/src/lib/supabase.ts:
 import { createClient as createBrowserClient } from '@supabase/supabase-js';
 export const supabase = createBrowserClient(
-  "https://ltjgxizrocesskzupixw.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0amd4aXpyb2Nlc3NrenVwaXh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcxMDE1NzEsImV4cCI6MjAxMjY3NzU3MX0.lUlIXbcuZX-1ZnX8G1QfxJlv7RkN63WzDgbh_xVjxss"
+  "",
+  ""
 );
